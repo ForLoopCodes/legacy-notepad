d1ee3bd0615bf8b881fbccd9ebabf094f6e85fd9
ForLoopCodes <meetnp1706@gmail.com>
2026-02-04 17:47:55 +0530

    fix word wrap theme issues

--- src/modules/editor.cpp
+++ src/modules/editor.cpp
@@ -17,6 +17,7 @@
 #include "core/types.h"
 #include "core/globals.h"
 #include "theme.h"
+#include "ui.h"
 #include "background.h"
 #include "resource.h"
 #include <richedit.h>
@@ -76,8 +77,6 @@ void ApplyFont()
                                 DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY,
                                 DEFAULT_PITCH | FF_DONTCARE, g_state.fontName.c_str());
     SendMessageW(g_hwndEditor, WM_SETFONT, reinterpret_cast<WPARAM>(g_state.hFont), TRUE);
-
-    // Ensure correct text color in dark mode after font change
     COLORREF textColor = IsDarkMode() ? RGB(255, 255, 255) : GetSysColor(COLOR_WINDOWTEXT);
     CHARFORMAT2W cf = {sizeof(cf)};
     cf.dwMask = CFM_COLOR;
@@ -100,21 +99,16 @@ void ApplyWordWrap()
     DWORD style = WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_WANTRETURN | ES_NOHIDESEL;
     if (!g_state.wordWrap)
         style |= WS_HSCROLL | ES_AUTOHSCROLL;
-    g_hwndEditor = CreateWindowExW(0, L"EDIT", nullptr, style,
+    g_hwndEditor = CreateWindowExW(0, MSFTEDIT_CLASS, nullptr, style,
                                    0, 0, 100, 100, g_hwndMain, reinterpret_cast<HMENU>(IDC_EDITOR), GetModuleHandleW(nullptr), nullptr);
+    g_origEditorProc = reinterpret_cast<WNDPROC>(SetWindowLongPtrW(g_hwndEditor, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(EditorSubclassProc)));
+    SendMessageW(g_hwndEditor, EM_SETLIMITTEXT, 0, 0);
+    SendMessageW(g_hwndEditor, EM_SETEVENTMASK, 0, ENM_CHANGE);
     ApplyFont();
+    ApplyTheme();
     SetEditorText(text);
     SendMessageW(g_hwndEditor, EM_SETSEL, start, end);
-    RECT rc;
-    GetClientRect(g_hwndMain, &rc);
-    int statusH = 0;
-    if (g_state.showStatusBar)
-    {
-        RECT rs;
-        GetWindowRect(g_hwndStatus, &rs);
-        statusH = rs.bottom - rs.top;
-    }
-    MoveWindow(g_hwndEditor, 0, 0, rc.right, rc.bottom - statusH, TRUE);
+    ResizeControls();
     SetFocus(g_hwndEditor);
 }
 
@@ -189,10 +183,20 @@ LRESULT CALLBACK EditorSubclassProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM l
         }
         break;
     case WM_CHAR:
-        if (wParam == 127)
+        if (wParam == 3)
+            break;
+        if (wParam == 22)
+            break;
+        if (wParam == 24)
+            break;
+        if (wParam == 26)
+            break;
+        if (wParam == 25)
+            break;
+        if (wParam == 127 || wParam == 8)
         {
-            DeleteWordBackward();
-            return 0;
+            if (GetKeyState(VK_CONTROL) & 0x8000)
+                return 0;
         }
         if (g_state.background.enabled && g_bgImage)
         {
@@ -222,6 +226,39 @@ LRESULT CALLBACK EditorSubclassProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM l
             return result;
         }
         break;
+    case WM_MOUSEWHEEL:
+    {
+        if (LOWORD(wParam) & MK_SHIFT)
+        {
+            int delta = GET_WHEEL_DELTA_WPARAM(wParam);
+            UINT scrollLines = 3;
+            SystemParametersInfoW(SPI_GETWHEELSCROLLLINES, 0, &scrollLines, 0);
+            if (scrollLines == (UINT)WHEEL_PAGESCROLL)
+            {
+                SendMessageW(hwnd, WM_HSCROLL, (delta > 0) ? SB_PAGELEFT : SB_PAGERIGHT, 0);
+            }
+            else
+            {
+                for (UINT i = 0; i < scrollLines; ++i)
+                    SendMessageW(hwnd, WM_HSCROLL, (delta > 0) ? SB_LINELEFT : SB_LINERIGHT, 0);
+            }
+            return 0;
+        }
+        break;
+    }
+    case WM_MOUSEHWHEEL:
+    {
+        int delta = GET_WHEEL_DELTA_WPARAM(wParam);
+        UINT scrollChars = 3;
+        SystemParametersInfoW(SPI_GETWHEELSCROLLCHARS, 0, &scrollChars, 0);
+        if (delta != 0)
+        {
+            for (UINT i = 0; i < scrollChars; ++i)
+                SendMessageW(hwnd, WM_HSCROLL, (delta > 0) ? SB_LINERIGHT : SB_LINELEFT, 0);
+            return 0;
+        }
+        break;
+    }
     }
     return CallWindowProcW(g_origEditorProc, hwnd, msg, wParam, lParam);
 }
